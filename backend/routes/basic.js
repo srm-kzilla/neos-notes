693b642f9747f9d02736d8f1595779a9a5d976f6
Alvin Ben <69302420+alvinbengeorge@users.noreply.github.com>
2024-09-09 00:13:18 +0530

    chore: update basic.js routes

--- backend/routes/basic.js
+++ backend/routes/basic.js
@@ -3,6 +3,6 @@ import { basic } from "../controllers/basic.js";
 
 const router = express.Router();
 
-router.get("/", basic);
+// TODO-4: Implement Routes for the basic controller here
 
 export default router;
